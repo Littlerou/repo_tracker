fe5c0ce7a1c1b96c484f88c00daa7b40a8c014f4
Bradley Neve <brad.neve@gmail,com>
2022-10-05 14:44:33 +0100

    show data of specific repo on screen

--- repo-tracker/src/pages/Repo/index.js
+++ repo-tracker/src/pages/Repo/index.js
@@ -1,18 +1,22 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import axios from "axios";
 
 function Repo() {
 
+    const [ repoData, setRepoData ] = useState([])
+
     const params = useParams()
     
-    const username = useSelector(state => state.username)
+    // const username = useSelector(state => state.username)
+    const username = 'littlerou'
     useEffect(() => {
         const fetchRepo = async () => {
             try {
                 const { data } = await axios.get(`https://api.github.com/repos/${username}/${params.repo}`)
-                console.log(data)
+                setRepoData(data)
+                console.log(repoData.forks)
             } catch (error) {
                 console.log(error)
 
@@ -25,10 +29,10 @@ function Repo() {
     <div className="content">
         <h1>{params.repo}</h1>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>Forks: {repoData.forks}</li>
+            <li>Stargazers: {repoData.stargazers_count}</li>
+            <li>Clone URL: {repoData.clone_url}</li>
+            <li>Open issues: {repoData.open_issues}</li>
         </ul>
     </div>
     
