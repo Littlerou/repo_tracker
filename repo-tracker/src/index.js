a407d6784ad0c80544b921b9624985079e699df8
Littlerou <izzymelvin4@gmail.com>
2022-10-05 10:49:57 +0100

    converted to redux

--- repo-tracker/src/index.js
+++ repo-tracker/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './store';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App'
+import { searchReducer } from './reducers';
 
 import './styles/index.css';
 
+const store = createStore(searchReducer);
 
 ReactDOM.render(
     <Provider store={store}>
